224beb0bff368b8e739aa6f310c56fafb0e100cf
ninetyeight9800 <ninetyeight9800@gmail.com>
2025-01-03 15:53:09 +0600

    new v

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "992648601"
+    serviceWorkerVersion: "2482911583"
   }
 });
